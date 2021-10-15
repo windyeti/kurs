524825885c39e229978679f6249e3cda50a2cce7
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-15 08:31:22 +0300

    add seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # [{period: "1", price: 100}, {period: "30", price: 600}, {period: "180", price: 1000}, {period: "навсегда", price: 3000}].each { |plan| Payplan.create!(plan) }
-# User.create(email: 'yegor.tikhanin@gmail.com', password: '123456', password_confirmation: "123456", role: 'Admin', subdomain: "egor_shop", name: "Egor")
+User.create(email: 'yegor.tikhanin@gmail.com', password: '123456', password_confirmation: "123456", role: 'Admin', subdomain: "egor_shop", name: "Egor")
 # User.create(email: 'soharta@list.ru', password: '123456', password_confirmation: "123456", subdomain: "soharta_shop", name: "Soharta")
 # Payplan.create(period: "12", price: 125, for: "ReviewIntegration")
