e058a4ed126ff3682853a4061aab2596cea7fa9f
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-15 10:27:46 +0300

    change session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_myappda_session', domain: {
-    production:   '.k-comment.ru',
+    production:   '167.172.160.127',
     staging:      '.k-comment.ru',
     development:  '.lvh.me'
     # development:  '.k-comment.ru'
