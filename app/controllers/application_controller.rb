469febf4a88bdd37b48f2ac6aec8e1b87a3a1801
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-19 08:16:39 +0300

    redirect_to subdomaim move to public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,16 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def redirect_to_subdomain
+    return if self.is_a?(DeviseController)
+    if request.subdomain.present?
+      if current_user.present? && request.subdomain != current_user.subdomain
+        subdomain = current_user.subdomain
+        host = request.host_with_port.sub!("#{request.subdomain}", subdomain)
+        redirect_to "http://#{host}#{request.path}"
+      end
+    end
+  end # redirect_to_subdomain
 
   protected
 
@@ -44,16 +54,6 @@ class ApplicationController < ActionController::Base
   end # invoice_path_for
 
 
-  def redirect_to_subdomain
-    return if self.is_a?(DeviseController)
-    if request.subdomain.present?
-      if current_user.present? && request.subdomain != current_user.subdomain
-        subdomain = current_user.subdomain
-        host = request.host_with_port.sub!("#{request.subdomain}", subdomain)
-        redirect_to "http://#{host}#{request.path}"
-      end
-    end
-  end # redirect_to_subdomain
 
   # def redirect_to_dashboard
   #   if current_user
