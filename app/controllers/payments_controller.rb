db8f6c6d6d10a79378b6b9313b81542bb8ada848
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-19 08:03:17 +0300

    Revert

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -48,7 +48,6 @@ class PaymentsController < ApplicationController
     end
     redirect_to root_path, notice: "Платеж принят"
   end
-
   # TODO после присвоения доменного имени надо вернуть redirect
   def success
     puts 'success here'
