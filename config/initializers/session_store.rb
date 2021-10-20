2ef90d5b2d67e761aba559da775b55d3db60c7bb
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-20 09:44:28 +0300

    fix session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,11 @@
 # Be sure to restart your server when you modify this file.
 # TODO 167.172.160.127 заменить на имя домена
-# Rails.application.config.session_store :cookie_store, key: '_myappda_session', domain: {
-#     production:   '167.172.160.127',
-#     staging:      '.k-comment.ru',
-#     development:  '.lvh.me'
-#     # development:  '.k-comment.ru'
-# }.fetch(Rails.env.to_sym, :all)
+Rails.application.config.session_store :cookie_store, key: '_myappda_session', domain: {
+    production:   '.irbandtest.ru',
+    staging:      '.k-comment.ru',
+    development:  '.lvh.me'
+    # development:  '.k-comment.ru'
+}.fetch(Rails.env.to_sym, :all)
 
-Kurs::Application.config.session_store :cookie_store, key: '_kurs_session', domain: :all, tld_length: 2
+# Rails.application.config.session_store :cookie_store, key: '_kurs_session', domain: :all, tld_length: 2
+# Kurs::Application.config.session_store :cookie_store, key: '_kurs_session', domain: :all, tld_length: 2
