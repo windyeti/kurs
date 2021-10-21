412410de8972fa79b2af8a3ea0495f0afe3f34e5
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-21 10:01:37 +0300

    chore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
     puts resource_or_scope.subdomain + " - это из ApplicationController - after_sign_in_path_for"
     # dashboard_index_url(subdomain: resource_or_scope.subdomain)
     Apartment::Tenant.switch(resource_or_scope.subdomain) do
-      root_path
+      dashboard_index_path
     end
   end # after_sign_in_path_for
 
