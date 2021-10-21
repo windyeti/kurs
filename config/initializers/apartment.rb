56272545e6e23c474da8de169e2cee5824ed858a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-21 10:35:39 +0300

    chore

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -2,7 +2,7 @@
 # Apartment can support many different "Elevators" that can take care of this routing to your data.
 # Require whichever Elevator you're using below or none if you have a custom one.
 #
-require 'apartment/elevators/generic'
+# require 'apartment/elevators/generic'
 # require 'apartment/elevators/domain'
 # require 'apartment/elevators/subdomain'
 require 'apartment/elevators/first_subdomain'
@@ -112,7 +112,7 @@ end
 
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
 # Rails.application.config.middleware.use Apartment::Elevators::Subdomain
-# Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
+Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
 #
 Rails.application.config.middleware.insert_before Warden::Manager, Apartment::Elevators::FirstSubdomain
