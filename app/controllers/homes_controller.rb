f833572b3c4d316f53fd56de42ec335025f46aa4
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-23 07:54:31 +0300

    home controller add redirect_to_app_url

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
-  before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:index, :manual]
+  before_action :redirect_to_app_url
+  skip_before_action :redirect_to_subdomain
 
   authorize_resource :class => false
 
