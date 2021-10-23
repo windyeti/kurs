848df5d8fb63c186c8fede223fe988d813e865d3
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-10-23 11:34:54 +0300

    add head ok  after result

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -45,6 +45,7 @@ class PaymentsController < ApplicationController
     @payments.each do |payment|
       payment.update(:status => 'Оплачен', :paymentdate => params['LMI_SYS_PAYMENT_DATE'], :paymentid => params['LMI_SYS_PAYMENT_ID'])
     end
+    head :ok
   end
   # TODO после присвоения доменного имени надо вернуть redirect
   def success
